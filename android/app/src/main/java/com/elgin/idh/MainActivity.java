a04a5ca1f3f11fced117f56597bab1d959e189dd
Miranda <mmmt.eng@uea.edu.br>
2022-07-12 09:45:38 -0400

    [feat] Project init

---
+++ android/app/src/main/java/com/elgin/idh/MainActivity.java
@@ -0,0 +1,5 @@
+package com.elgin.idh;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.elgin.idh',
   appName: 'M8_IDH',
   webDir: 'www',
   bundledWebRuntime: false
