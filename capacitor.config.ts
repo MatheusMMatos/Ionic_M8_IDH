04281e9da5a7840cc0104d8ae2d03977181b11aa
Miranda <mmmt.eng@uea.edu.br>
2022-07-12 09:18:23 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'M8_IDH',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
